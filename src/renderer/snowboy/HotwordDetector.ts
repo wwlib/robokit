dd28dfaec77bf49ce736f290902cbc6f6555c675
wwlib <andrew@wwlib.org>
2018-07-09 13:25:29 -0400

    got snowboy working on mac

--- src/renderer/snowboy/HotwordDetector.ts
+++ src/renderer/snowboy/HotwordDetector.ts
@@ -1,10 +1,15 @@
-import { Detector, Models } from "snowboy";
+import { Detector, Models } from 'snowboy';
+const path = require('path');
+const findRoot = require('find-root');
+
+const root = findRoot(__dirname);
+const commonResPath: string = path.resolve(root, '/resources/common.res')
 
 export default class HotwordDetector extends Detector {
 
 	constructor(models: Models) {
 		super({
-	           resource: `${process.env.CWD}/resources/common.res`,
+	           resource: commonResPath,
 	           models: models,
 	           audioGain: 2.0,
 		});
@@ -16,17 +21,17 @@ export default class HotwordDetector extends Detector {
 	      // handle silent state
 		console.log("silence");
 	   });
-	
+
 	   this.on("sound", () => {
 	      // handle sound detected state
 		console.log("sound");
 	   });
-	
+
 	   this.on("error", (error) => {
 	      // handle error
 		console.log("error", error);
 	   });
-	
+
 	   this.on("hotword", (index, hotword) => {
 	      // hotword detected
 		console.log("hotword");
--- src/renderer/snowboy/SnowboyController.ts
+++ src/renderer/snowboy/SnowboyController.ts
@@ -1,14 +1,59 @@
-import * as record from 'node-record-lpcm16';
-import HotwordDetector from './HotwordDetector';
+const record = require('node-record-lpcm16');
+import { Detector, Models } from 'snowboy';
+// import HotwordDetector from './HotwordDetector';
+const path = require('path');
+const findRoot = require('find-root');
+
+const root = findRoot(__dirname);
+const modelPath: string = path.resolve(root, 'resources/models/snowboy.umdl');
+const commonResPath: string = path.resolve(root, 'resources/common.res');
+console.log(modelPath);
+console.log(commonResPath);
 
 export default class SnowboyController {
-	
-	public detector: HotwordDetector;
+
+	public models: Models
+	public detector: Detector;
 	public mic: any;
 
 	constructor() {
+		this.models = new Models();
+		this.models.add({
+		  file: modelPath,
+		  sensitivity: '0.5',
+		  hotwords : 'snowboy'
+		});
 
-		this.detector = new HotwordDetector();
+		this.detector = new Detector({
+		  resource: commonResPath,
+		  models: this.models,
+		  audioGain: 2.0,
+		  applyFrontend: true
+		});
+
+		this.detector.on('silence', function () {
+		  // console.log('silence');
+		});
+
+		this.detector.on('sound', function (buffer) {
+		  // <buffer> contains the last chunk of the audio that triggers the "sound"
+		  // event. It could be written to a wav stream.
+		  // console.log('sound');
+		});
+
+		this.detector.on('error', function () {
+		  console.log('error');
+		});
+
+		this.detector.on('hotword', function (index, hotword, buffer) {
+		  // <buffer> contains the last chunk of the audio that triggers the "hotword"
+		  // event. It could be written to a wav stream. You will have to use it
+		  // together with the <buffer> in the "sound" event if you want to get audio
+		  // data after the hotword.
+		  console.log(buffer);
+		  console.log('hotword', index, hotword);
+		});
+
 		this.mic = record.start({
 		   threshold: 0,
 		   sampleRate: 16000,
@@ -16,7 +61,7 @@ export default class SnowboyController {
 		});
 
 		this.mic.pipe(this.detector as any);
-	
+
 	}
 
 }

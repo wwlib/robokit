3dd015c3da24c98c205f10616054b45b2b1ced76
Andrew Rapo <andrew@wwlib.org>
2019-08-16 22:23:15 -0400

    added src/renderer/AppVersion.ts

---
+++ src/renderer/AppVersion.ts
@@ -0,0 +1,2 @@
+// This file was generated on Thu Mar  7 06:36:18 EST 2019
+export const appVersion = '0.0.2';
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-version_file=src/renderer/model/AppVersion.ts
+version_file=src/renderer/AppVersion.ts
 > $version_file
 echo "// This file was generated on $(date)
 export const appVersion = '$1';" >> $version_file
